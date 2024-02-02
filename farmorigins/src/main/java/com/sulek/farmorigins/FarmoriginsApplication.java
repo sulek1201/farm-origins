fbffde4da71c4ebf4c793a8100200c56889f4a6b
Anıl Şülekoğlu <39844117+sulek1201@users.noreply.github.com>
2024-02-02 14:28:41 +0300

    Add files via upload

---
+++ farmorigins/src/main/java/com/sulek/farmorigins/FarmoriginsApplication.java
@@ -0,0 +1,13 @@
+package com.sulek.farmorigins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmoriginsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmoriginsApplication.class, args);
+	}
+
+}
---
+++ farmorigins/src/test/java/com/sulek/farmorigins/FarmoriginsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulek.farmorigins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmoriginsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
